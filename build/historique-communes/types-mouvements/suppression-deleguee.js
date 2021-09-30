121bd380a6ec8f0609b6bde23889428f42c9749a
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-09-30 12:06:59 +0200

    Improve suppression-deleguee (not sure)

--- build/historique-communes/types-mouvements/suppression-deleguee.js
+++ build/historique-communes/types-mouvements/suppression-deleguee.js
@@ -1,13 +1,8 @@
-const {chain} = require('lodash')
 const {mouvementToCommune} = require('../helpers')
 
 module.exports = function (mouvements, model) {
-  chain(mouvements)
-    .filter(m => m.TYPECOM_AV === 'COMD')
-    .uniqBy('COM_AV')
-    .forEach(m => {
-      const commune = model.getCommuneOrInit(mouvementToCommune(m, 'AV'))
-      model.end(commune, 'suppression de la commune déléguée')
-    })
-    .value()
+  mouvements.forEach(m => {
+    const communeDeleguee = model.getCommuneOrInit({...mouvementToCommune(m, 'AV'), type: 'COMD'})
+    model.createSuccessor(communeDeleguee, {type: 'COMP'}, 'suppression de la commune déléguée', true)
+  })
 }
