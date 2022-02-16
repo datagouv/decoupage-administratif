456448ae802ada571c41b767c1eb14a807793818
Thomas Gratier <thomas_gratier@yahoo.fr>
2022-02-16 13:59:56 +0100

    Delete dir build/historique-communes

--- build/historique-communes/types-mouvements/changement-departement.js
+++
@@ -1,8 +0,0 @@
-const {mouvementToCommune} = require('../helpers')
-
-module.exports = function (mouvements, model) {
-  mouvements.forEach(m => {
-    const communeOrigine = model.getCommuneOrInit(mouvementToCommune(m, 'AV'))
-    model.createSuccessor(communeOrigine, {code: m.COM_AP}, 'changement de département')
-  })
-}
